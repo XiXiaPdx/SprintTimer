de78857ea356d415a03903ae3ff51ee3e70a1cea
Xi Xia <xxia@newrelic.com>
2018-12-23 00:12:32 -0800

    openCV might be creating foreground mask

--- SprintTimer/OpenCVWrapper.mm
+++ SprintTimer/OpenCVWrapper.mm
@@ -8,22 +8,35 @@
 
 #import <opencv2/opencv.hpp>
 #import <opencv2/imgcodecs/ios.h>
+
 #import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
 
 @implementation OpenCVWrapper : NSObject
 
+
+
+
 +(UIImage *)ConvertImage:(UIImage *)image {
   cv::Mat mat;
+  
+  // motion detection stuff
+  cv::Mat fgMask;
+  cv::Ptr<cv::BackgroundSubtractor> pBackSub;
+  
+  pBackSub = cv::createBackgroundSubtractorMOG2();
+
   UIImageToMat(image, mat);
   
-  cv::Mat gray;
-  cv::cvtColor(mat, gray, CV_RGB2GRAY);
+  pBackSub->apply(mat, fgMask);
   
-  cv::Mat bin;
-  cv::threshold(gray, bin, 0, 255, cv::THRESH_BINARY | cv::THRESH_OTSU);
+//  cv::Mat gray;
+//  cv::cvtColor(mat, gray, CV_RGB2GRAY);
+//
+//  cv::Mat bin;
+//  cv::threshold(gray, bin, 0, 255, cv::THRESH_BINARY | cv::THRESH_OTSU);
   
-  UIImage *binImg = MatToUIImage(bin);
+  UIImage *binImg = MatToUIImage(fgMask);
   return binImg;
 }
 
