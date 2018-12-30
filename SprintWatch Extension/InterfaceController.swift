019f1d4be827e0f8cc65190ecd2cef90cfa8f48a
Xi Xia <xxia@newrelic.com>
2018-12-30 12:19:46 -0800

    accelerometer accessed and pshowing X acceleration on screen

--- SprintWatch Extension/InterfaceController.swift
+++ SprintWatch Extension/InterfaceController.swift
@@ -8,14 +8,46 @@
 
 import WatchKit
 import Foundation
+import CoreMotion
 
 
 class InterfaceController: WKInterfaceController {
-
+  
+  @IBOutlet var X_Accel_Label: WKInterfaceLabel!
+  
+  let motionManager: CMMotionManager = CMMotionManager()
+  let queue = OperationQueue()
+  let wristLocationIsLeft = WKInterfaceDevice.current().wristLocation == .left
+  let sampleInterval = 0.01
+  
     override func awake(withContext context: Any?) {
         super.awake(withContext: context)
         
         // Configure interface objects here.
+      guard motionManager.isAccelerometerAvailable else {
+        print("CM Motion manager not available")
+        return
+      }
+      
+      motionManager.accelerometerUpdateInterval = sampleInterval
+      motionManager.startAccelerometerUpdates(to: .main) {
+        [weak self] (data, error) in
+        guard let data = data, error == nil else {
+          return
+        }
+        let numberFormatter = NumberFormatter()
+        numberFormatter.numberStyle = .decimal
+        let num = NSNumber(value: data.acceleration.x)
+        let finalNumber = numberFormatter.string(from: num)
+
+       
+        self?.X_Accel_Label.setText(finalNumber)
+        
+       
+        
+        // ...
+      }
+      
     }
     
     override func willActivate() {
