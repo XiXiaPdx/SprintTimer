77608d06ade6273c3ca20adfa40f728bee95eae0
Xi Xia <xxia@newrelic.com>
2019-01-27 13:56:06 -0800

    added CameraHelper class to pass unit test check for existence of this class

---
+++ SprintTimerRedo/CameraHelper.swift
@@ -0,0 +1,13 @@
+//
+//  CameraHelper.swift
+//  SprintTimerRedo
+//
+//  Created by Xi Xia on 1/27/19.
+//  Copyright © 2019 Xi Xia. All rights reserved.
+//
+
+import Foundation
+
+class CameraHelper {
+  
+}
--- SprintTimerRedoTests/SprintTimerRedoTests.swift
+++ SprintTimerRedoTests/SprintTimerRedoTests.swift
@@ -20,15 +20,10 @@ class SprintTimerRedoTests: XCTestCase {
     }
 
     func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+        // Test the existence of class CameraHelper
+      let camera = CameraHelper()
+      XCTAssertNotNil(camera)
+      
     }
 
 }
